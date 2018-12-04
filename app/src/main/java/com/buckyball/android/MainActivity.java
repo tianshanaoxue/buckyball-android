6e2fb62fb5c8451c0e09fab5003e97a5165678d0
liuweies <574613441@qq.com>
2018-12-04 17:50:52 +0800

    加数字键盘module

--- app/src/main/java/com/buckyball/android/MainActivity.java
+++ app/src/main/java/com/buckyball/android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.buckyball.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_numberkeyboard){
+            Intent intent = new Intent(this,BbNkbMainActivity.class);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ buckyball_numberkeyboard/src/main/java/com/buckyball/module/numberkeyboard/BbNkbMainActivity.java
@@ -0,0 +1,18 @@
+package com.buckyball.module.numberkeyboard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author liuwei
+ * @desc
+ * @date 2018/12/4 16:48
+ * @email liuweies@didichuxing.com
+ */
+public class BbNkbMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
